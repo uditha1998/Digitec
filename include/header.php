6c9eb735deca858b60096665b56c79a6855b8ab7
hirusha2577 <56668123+hirusha2577@users.noreply.github.com>
2021-12-14 11:26:05 +0530

    #1 Add Temp Folder

---
+++ include/header.php
@@ -0,0 +1,159 @@
+<div id="header" class="section header-section transparent-header">
+            <div class="container">
+
+                <!-- Header Wrap Start  -->
+                <div class="header-wrap">
+
+                    <div class="header-logo">
+                        <a href="index-2.html"><img src="assets/images/logo/digitech-logo.jpeg" alt=""></a>
+                    </div>
+                   
+                    <div class="header-menu d-none d-lg-block">
+                        <ul class="main-menu">
+                            <li class="active-menu">
+                                <a href="index-2.html">Home</a>
+                                <ul class="sub-menu">
+                                    <li><a href="index-2.html">Home One</a></li>
+                                    <li><a href="index-3.html">Home Two</a></li>
+                                    <li><a href="index-4.html">Home Three</a></li>
+                                    <li><a href="index-5.html">Home Four</a></li>
+                                    <li class="active"><a href="index-6.html">Home Five</a></li>
+                                    <li><a href="index-7.html">Home Six</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="about.html">Aboute Us</a>
+                            </li>
+                            <li><a href="#">Pages</a>
+                                <ul class="sub-menu">
+                                    <li><a href="team.html">Our Team</a></li>
+                                    <li><a href="service.html">Service</a></li>
+                                    <li><a href="why-choose.html">Why Choose Us</a></li>
+                                    <li><a href="testimonial.html">Testimonial</a></li>
+                                    <li><a href="pricing.html">Pricing</a></li>
+                                    <li><a href="login-register.html">Login & Register</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Blog</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog Grid</a></li>
+                                    <li><a href="blog-standard.html">Latest News</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- Header Meta Start -->
+                    <div class="header-meta">
+                        <!-- Header Info Start -->
+                        <div class="header-info d-none d-md-flex">
+                            <div class="info-icon">
+                                <i class="fas fa-phone-alt"></i>
+                            </div>
+                            <div class="info-text">
+                                <span>Free Consultant</span>
+                                <span class="number"><a href="#">+36 55 540 069</a></span>
+                            </div>
+                        </div>
+                        <!-- Header Info End -->
+                        <!-- Header Search Start -->
+                        <div class="header-search">
+                            <a class="search-btn" href="#"><i class="flaticon-loupe"></i></a>
+                            <div class="search-wrap">
+                                <div class="search-inner">
+                                    <i id="search-close" class="flaticon-close search-close"></i>
+                                    <div class="search-cell">
+                                        <form action="#">
+                                            <div class="search-field-holder">
+                                                <input class="main-search-input" type="search" placeholder="Search Your Keyword...">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Header Search End -->
+
+                        <!-- Header Cart Start -->
+                        <div class="header-cart dropdown">
+                            <button class="cart-btn" data-bs-toggle="dropdown">
+                                <i class="flaticon-shopping-cart"></i>
+                                <span class="count">0</span>
+                            </button>
+                            <!-- Header Dropdown Cart Start -->
+                            <div class="dropdown-menu dropdown-cart">
+                                <!-- Dropdown Cart Items Start -->
+                                <div class="cart-items">
+                                    <!-- Single Cart Item Start -->
+                                    <div class="single-cart-item">
+                                        <div class="item-image">
+                                            <img src="assets/images/shop-cart-1.jpg" alt="cart">
+                                        </div>
+                                        <div class="item-content">
+                                            <h4 class="title"><a href="#">Apple Iphone X</a></h4>
+                                            <span class="quantity">2 x $59.99</span>
+                                        </div>
+                                        <button class="btn-close"></button>
+                                    </div>
+                                    <!-- Single Cart Item End -->
+                                    <!-- Single Cart Item Start -->
+                                    <div class="single-cart-item">
+                                        <div class="item-image">
+                                            <img src="assets/images/shop-cart-2.jpg" alt="cart">
+                                        </div>
+                                        <div class="item-content">
+                                            <h4 class="title"><a href="#">Sony Xperia Tablet</a></h4>
+                                            <span class="quantity">2 x $59.99</span>
+                                        </div>
+                                        <button class="btn-close"></button>
+                                    </div>
+                                    <!-- Single Cart Item End -->
+                                    <!-- Single Cart Item Start -->
+                                    <div class="single-cart-item">
+                                        <div class="item-image">
+                                            <img src="assets/images/shop-cart-3.jpg" alt="cart">
+                                        </div>
+                                        <div class="item-content">
+                                            <h4 class="title"><a href="#">Camera Digital</a></h4>
+                                            <span class="quantity">2 x $59.99</span>
+                                        </div>
+                                        <button class="btn-close"></button>
+                                    </div>
+                                    <!-- Single Cart Item End -->
+                                </div>
+                                <!-- Dropdown Cart Items End -->
+                                <!-- Dropdown Cart Total Start -->
+                                <div class="cart-total">
+                                    <span class="label">Subtotal:</span>
+                                    <span class="value">0</span>
+                                </div>
+                                <!-- Dropdown Cart Total End -->
+                                <!-- Dropdown Cart Button Start -->
+                                <div class="cart-btns">
+                                    <a class="btn" href="#">View Cart</a>
+                                    <a class="btn btn-2" href="#">Checkout</a>
+                                </div>
+                                <!-- Dropdown Cart Button End -->
+                            </div>
+                            <!-- Header Dropdown Cart End -->
+                        </div>
+                        <!-- Header Cart End -->
+                        <!-- Header Toggle Start -->
+                        <div class="header-toggle d-lg-none">
+                            <button data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                        <!-- Header Toggle End -->
+                    </div>
+                    <!-- Header Meta End  -->
+
+                </div>
+                <!-- Header Wrap End  -->
+
+            </div>
+        </div>

26d6495e38873389fadeb0a313435904e484862a
uditha1998 <uditha.synotec@gmail.com>
2021-12-15 14:39:25 +0530

    #18 Create Service Inner Page

---
+++ service.php
@@ -0,0 +1,337 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+    <!-- Mirrored from thepixelcurve.com/html/techmax/techmax/service.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 13 Dec 2021 06:36:29 GMT -->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Techmax - Technology IT Solutions Consultancy HTML5 Template</title>
+        <meta name="robots" content="noindex, follow" />
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+
+        <!-- CSS
+            ============================================ -->
+
+        <!-- Icon Font CSS -->
+        <link rel="stylesheet" href="assets/css/plugins/all.min.css">
+        <link rel="stylesheet" href="assets/css/plugins/flaticon.css">
+
+        <!-- Plugins CSS -->
+        <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
+        <link rel="stylesheet" href="assets/css/plugins/aos.css">
+        <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+
+        <!-- Main Style CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+
+
+        <style>
+            .page-banner-section {
+
+            }
+        </style>
+
+    </head>
+
+    <body>
+
+        <div class="main-wrapper">
+
+
+            <!-- Preloader start -->
+            <div id="preloader">
+                <div class="preloader">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <!-- Preloader End -->
+
+            <!-- Header Start  -->
+            <?php
+            include_once './include/header.php';
+            ?>
+            <!-- Header End -->
+
+            <!-- Offcanvas Start-->
+            <div class="offcanvas offcanvas-start" id="offcanvasExample">
+                <div class="offcanvas-header">
+                    <!-- Offcanvas Logo Start -->
+                    <div class="offcanvas-logo">
+                        <a href="index-2.html"><img src="assets/images/logo-white.png" alt=""></a>
+                    </div>
+                    <!-- Offcanvas Logo End -->
+                    <button type="button" class="close-btn" data-bs-dismiss="offcanvas"><i class="flaticon-close"></i></button>
+                </div>
+
+                <!-- Offcanvas Body Start -->
+                <div class="offcanvas-body">
+                    <div class="offcanvas-menu">
+                        <ul class="main-menu">
+                            <li>
+                                <a href="index-2.html">Home</a>
+                                <ul class="sub-menu">
+                                    <li><a href="index-2.html">Home One</a></li>
+                                    <li><a href="index-3.html">Home Two</a></li>
+                                    <li><a href="index-4.html">Home Three</a></li>
+                                    <li><a href="index-5.html">Home Four</a></li>
+                                    <li><a href="index-6.html">Home Five</a></li>
+                                    <li><a href="index-7.html">Home Six</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="about.html">Aboute Us</a>
+                            </li>
+                            <li class="active-menu"><a href="#">Pages</a>
+                                <ul class="sub-menu">
+                                    <li><a href="team.html">Our Team</a></li>
+                                    <li class="active"><a href="service.html">Service</a></li>
+                                    <li><a href="why-choose.html">Why Choose Us</a></li>
+                                    <li><a href="testimonial.html">Testimonial</a></li>
+                                    <li><a href="pricing.html">Pricing</a></li>
+                                    <li><a href="login-register.html">Login & Register</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Blog</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog Grid</a></li>
+                                    <li><a href="blog-standard.html">Latest News</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Offcanvas Body End -->
+            </div>
+            <!-- Offcanvas End -->
+
+
+            <!-- Page Banner Start -->
+            <div class="section page-banner-section" style="background-image: url(assets/images/bg/cta-4-bg.jpg);">
+                <div class="shape-2"></div>
+                <div class="container">
+                    <div class="page-banner-wrap">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <!-- Page Banner Content Start -->
+                                <div class="page-banner text-center">
+                                    <h2 class="title">IT Services</h2>
+                                    <ul class="breadcrumb justify-content-center">
+                                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">IT Services</li>
+                                    </ul>
+                                </div>
+                                <!-- Page Banner Content End -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Page Banner End -->
+
+            <!-- Features Start -->
+            <div class="section features-section-3 features-section-4 section-padding service-section-padding">
+                <div class="container">
+                    <!-- Features Wrap Start -->
+                    <div class="features-wrap-3">
+                        <div class="section-title2 text-center">
+                            <h2 class="title">We run all kinds of <span>IT services</span> that vow your success</h2>
+                        </div>
+                        <!-- Service-2 Start -->
+                        <div class="section service-section-2 section-padding-03 service-page-padding" >
+                            <div class="container">
+                                <!-- Service-2 Wrap Start -->
+                                <div class="service-wrap-2">
+
+                                    <!-- Service-2 Content Wrap Start -->
+                                    <div class="service-content-wrap-2">
+                                        <div class="row">
+
+
+
+
+                                            <!-- Service Item Start -->
+                                            <div class="col-md-4">
+                                                <div class="service-item">
+                                                    <div class="service-img">
+                                                        <div class="shape-1"></div>
+                                                        <div class="shape-2"></div>
+                                                        <a class="image" href="#"><img src="assets/images/service/s-img-1.jpg" alt=""></a>
+                                                        <div class="service-img-text text-center">
+                                                            <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="service-content">
+                                                        <h3 class="title"><a href="#">Strategy & Discovery</a></h3>
+                                                        <a class="read-more" href="#"><i class="fas fa-long-arrow-alt-right"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <!-- Service Item End -->
+
+                                            <!-- Service Item Start -->
+                                            <div class="col-md-4">
+                                                <div class="service-item">
+                                                    <div class="service-img">
+                                                        <div class="shape-1"></div>
+                                                        <div class="shape-2"></div>
+                                                        <a class="image" href="#"><img src="assets/images/service/s-img-2.jpg" alt=""></a>
+                                                        <div class="service-img-text text-center">
+                                                            <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="service-content">
+                                                        <h3 class="title"><a href="#">Software Solution</a></h3>
+                                                        <a class="read-more" href="#"><i class="fas fa-long-arrow-alt-right"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>  
+                                            <!-- Service Item End -->
+
+                                            <!-- Service Item Start -->
+                                            <div class="col-md-4">
+                                                <div class="service-item">
+                                                    <div class="service-img">
+                                                        <div class="shape-1"></div>
+                                                        <div class="shape-2"></div>
+                                                        <a class="image" href="#"><img src="assets/images/service/s-img-3.jpg" alt=""></a>
+                                                        <div class="service-img-text text-center">
+                                                            <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="service-content">
+                                                        <h3 class="title"><a href="#">Technology Consulting</a></h3>
+                                                        <a class="read-more" href="#"><i class="fas fa-long-arrow-alt-right"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <!-- Service Item End -->
+                                            <!-- Service Item Start -->
+                                            <div class="col-md-4">
+                                                <div class="service-item">
+                                                    <div class="service-img">
+                                                        <div class="shape-1"></div>
+                                                        <div class="shape-2"></div>
+                                                        <a class="image" href="#"><img src="assets/images/service/s-img-1.jpg" alt=""></a>
+                                                        <div class="service-img-text text-center">
+                                                            <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="service-content">
+                                                        <h3 class="title"><a href="#">Strategy & Discovery</a></h3>
+                                                        <a class="read-more" href="#"><i class="fas fa-long-arrow-alt-right"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <!-- Service Item End -->
+
+                                            <!-- Service Item Start -->
+                                            <div class="col-md-4">
+                                                <div class="service-item">
+                                                    <div class="service-img">
+                                                        <div class="shape-1"></div>
+                                                        <div class="shape-2"></div>
+                                                        <a class="image" href="#"><img src="assets/images/service/s-img-2.jpg" alt=""></a>
+                                                        <div class="service-img-text text-center">
+                                                            <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="service-content">
+                                                        <h3 class="title"><a href="#">Software Solution</a></h3>
+                                                        <a class="read-more" href="#"><i class="fas fa-long-arrow-alt-right"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>  
+                                            <!-- Service Item End -->
+
+                                            <!-- Service Item Start -->
+                                            <div class="col-md-4">
+                                                <div class="service-item">
+                                                    <div class="service-img">
+                                                        <div class="shape-1"></div>
+                                                        <div class="shape-2"></div>
+                                                        <a class="image" href="#"><img src="assets/images/service/s-img-3.jpg" alt=""></a>
+                                                        <div class="service-img-text text-center">
+                                                            <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="service-content">
+                                                        <h3 class="title"><a href="#">Technology Consulting</a></h3>
+                                                        <a class="read-more" href="#"><i class="fas fa-long-arrow-alt-right"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <!-- Service Item End -->
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!-- Service-2 Content Wrap End -->
+                            </div>
+                            <!-- Service-2 Wrap End -->
+                           
+                        </div>
+                    </div>
+                    <!-- Service-2 End -->
+                </div>
+                <!-- Features Wrap End -->
+            </div>
+        </div>
+        <!-- Features End -->
+
+
+
+        <?php
+        include_once './include/footer.php';
+        ?>
+        <!-- back to top start -->
+        <div class="progress-wrap">
+            <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+            </svg>
+        </div>
+        <!-- back to top end -->
+
+    </div>
+
+    <!-- JS
+    ============================================ -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="assets/js/plugins/popper.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/aos.js"></script>
+    <script src="assets/js/plugins/waypoints.min.js"></script>
+    <script src="assets/js/plugins/back-to-top.js"></script>
+    <script src="assets/js/plugins/jquery.counterup.min.js"></script>
+    <script src="assets/js/plugins/appear.min.js"></script>
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+

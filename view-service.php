3843b04ef026d4a16e25487a95a62834660eac95
uditha1998 <uditha.synotec@gmail.com>
2021-12-16 13:52:22 +0530

    #21 Create Service View Page

---
+++ view-service.php
@@ -0,0 +1,255 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Contact</title>
+        <meta name="robots" content="noindex, follow" />
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+
+
+
+        <!-- Icon Font CSS -->
+        <link rel="stylesheet" href="assets/css/plugins/all.min.css">
+        <link rel="stylesheet" href="assets/css/plugins/flaticon.css">
+
+        <!-- Plugins CSS -->
+        <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
+        <link rel="stylesheet" href="assets/css/plugins/aos.css">
+        <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+
+        <!-- Main Style CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <style>
+          
+        </style>
+
+
+    </head>
+
+    <body>
+
+        <div class="main-wrapper">
+
+
+            <!-- Preloader start -->
+            <div id="preloader">
+                <div class="preloader">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <!-- Preloader End -->
+
+            <!-- Header Start  -->
+            <?php
+            include './include/header.php';
+            ?>
+            <!-- Header End -->
+
+
+
+
+        <!-- Page Banner Start -->
+        <div class="section page-banner-section" style="background-image: url(assets/images/page-banner.jpg);">
+            <div class="shape-2"></div>
+            <div class="container">
+                <div class="page-banner-wrap">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <!-- Page Banner Content Start -->
+                            <div class="page-banner text-center">
+                                <h2 class="title">Blog Details</h2>
+                                <ul class="breadcrumb justify-content-center">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Blog Details</li>
+                                </ul>
+                            </div>
+                            <!-- Page Banner Content End -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Banner End -->
+
+        <!-- Blog Details Start -->
+        <div class="section blog-details-section section-padding ptb-75">
+            <div class="container">
+                <!-- Blog Details Wrap Start -->
+                <div class="blog-details-wrap">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <!-- Blog Details Post Start -->
+                            <div class="blog-details-post">
+                                <!-- Single Blog Start -->
+                                <div class="single-blog">
+                                    <div class="blog-img">
+                                        <a href="#"><img src="assets/images/blog/blog-s-1.jpg" alt=""></a>
+                                    
+                                    </div>
+                                    <div class="blog-content">
+                                        
+                                        <h3 class="title">Easy and Most Powerful Server and Platform.</h3>
+                                        <p>Accelerate innovation with world-class tech teams We’ll match you to an entire remote innovation with world-class tech teams We’ll match you to an entire remote team of incredible freelance talent for all your software development needs. The basic premise of search engine reputation management is to use the following three strategies to accomplish the goal of creating a completely positive first page of search engine results for a specific term….</p>
+                                    </div>
+                                </div>
+                                <!-- Single Blog End -->
+                         
+                            </div>
+                            <!-- Blog Details Post End -->
+                        </div>
+                        <div class="col-lg-4">
+                            <!-- Blog Sidebar Start -->
+                            <div class="blog-sidebar">
+          
+
+                                <!-- Sidebar Widget Start -->
+                                <div class="sidebar-widget">
+                                    <!-- Widget Title Start -->
+                                    <div class="widget-title">
+                                        <h3 class="title">Other Services</h3>
+                                    </div>
+                                    <!-- Widget Title End -->
+                                    <!-- Widget Recent Post Start -->
+                                    <div class="recent-posts">
+                                        <ul>
+                                            <li>
+                                                <a class="post-link" href="#">
+                                                    <div class="post-thumb">
+                                                        <img src="assets/images/blog/r-post-1.jpg" alt="">
+                                                    </div>
+                                                    <div class="post-text">
+                                                        <h4 class="title">Cupidatat non proident sunt culpa</h4>
+                                                        <span class="post-meta"><i class="far fa-calendar-alt"></i> May 15, 2020</span>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="post-link" href="#">
+                                                    <div class="post-thumb">
+                                                        <img src="assets/images/blog/r-post-2.jpg" alt="">
+                                                    </div>
+                                                    <div class="post-text">
+                                                        <h4 class="title">Cupidatat non proident sunt culpa</h4>
+                                                        <span class="post-meta"><i class="far fa-calendar-alt"></i> May 15, 2020</span>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="post-link" href="#">
+                                                    <div class="post-thumb">
+                                                        <img src="assets/images/blog/r-post-3.jpg" alt="">
+                                                    </div>
+                                                    <div class="post-text">
+                                                        <h4 class="title">Cupidatat non proident sunt culpa</h4>
+                                                        <span class="post-meta"><i class="far fa-calendar-alt"></i> May 15, 2020</span>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <!-- Widget Recent Post End -->
+                                </div>
+                                <!-- Sidebar Widget End -->
+
+                                <!-- Sidebar Widget Start -->
+                                <div class="sidebar-widget">
+                                    <!-- Widget Title Start -->
+                                    <div class="widget-title">
+                                        <h3 class="title">Categories</h3>
+                                    </div>
+                                    <!-- Widget Title End -->
+                                    <!-- Widget Category Start -->
+                                    <ul class="category">
+                                        <li class="cate-item"><a href="#"><i class="flaticon-next"></i> Technology <span class="post-count">3</span></a></li>
+                                        <li class="cate-item"><a href="#"><i class="flaticon-next"></i> Innovation <span class="post-count">5</span></a></li>
+                                        <li class="cate-item"><a href="#"><i class="flaticon-next"></i> Learning <span class="post-count">3</span></a></li>
+                                        <li class="cate-item"><a href="#"><i class="flaticon-next"></i> Information <span class="post-count">3</span></a></li>
+                                    </ul>
+                                    <!-- Widget Category End -->
+                                </div>
+                                <!-- Sidebar Widget End -->
+
+                                <!-- Sidebar Widget Start -->
+                                <div class="sidebar-widget">
+                                    <!-- Widget Title Start -->
+                                    <div class="widget-title">
+                                        <h3 class="title">Tags</h3>
+                                    </div>
+                                    <!-- Widget Title End -->
+                                    <!-- Widget Category Start -->
+                                    <ul class="sidebar-tag">
+                                        <li><a href="#">Learning</a></li>
+                                        <li><a href="#">Course</a></li>
+                                        <li><a href="#">Business</a></li>
+                                        <li><a href="#">Udemy</a></li>
+                                        <li><a href="#">Online</a></li>
+                                        <li><a href="#">Technology</a></li>
+                                    </ul>
+                                    <!-- Widget Category End -->
+                                </div>
+                                <!-- Sidebar Widget End -->
+                            </div>
+                            <!-- Blog Sidebar End -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Blog Details Wrap End -->
+            </div>
+        </div>
+        <!-- Blog Details End -->
+
+            <!-- Footer Section Start -->
+            <?php
+            include './include/footer.php';
+            ?>
+            <!-- Footer Section End -->
+
+            <!-- back to top start -->
+            <div class="progress-wrap">
+                <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+                <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+                </svg>
+            </div>
+            <!-- back to top end -->
+
+        </div>
+
+        <!-- JS
+        ============================================ -->
+        <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+
+        <!-- Bootstrap JS -->
+        <script src="assets/js/plugins/popper.min.js"></script>
+        <script src="assets/js/plugins/bootstrap.min.js"></script>
+
+        <!-- Plugins JS -->
+        <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+        <script src="assets/js/plugins/aos.js"></script>
+        <script src="assets/js/plugins/waypoints.min.js"></script>
+        <script src="assets/js/plugins/back-to-top.js"></script>
+        <script src="assets/js/plugins/jquery.counterup.min.js"></script>
+        <script src="assets/js/plugins/appear.min.js"></script>
+        <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+
+
+
+
+        <!-- Main JS -->
+        <script src="assets/js/main.js"></script>
+
+    </body>
+
+
+
+</html>
